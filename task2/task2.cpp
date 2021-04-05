09832372b75f7bedc0448966fcbecc9dd8f51408
vladimashev <vladimashev@mail.ru>
2021-04-06 02:31:57 +0300

    Task2 code

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -2,25 +2,48 @@
 #include <complex>
 #include "stdlib.h"
 #include <mpi.h>
-// command line:  ./Task1 n k mode filename
+
+#define EPS 10e-5
+#define RESFILE "result"
+// command line:  ./Task1 n k mode filename (for reading the vector)
 // argv              0    1 2  3       4
 typedef std::complex<double> complexd;
 
 
-void OneQubitEvolution(complexd *in, complexd *out, complexd *U, long long length, int n, int k)
+void OneQubitEvolution(complexd *in, complexd *out, complexd *U, int n, int k, long long size)
 {
+    //size here is a count of elements per process
+    int first_bit = rank * size;
     int shift = n - k;
-    int mask = 1 << shift;
-    int i;
-    #pragma omp parallel default(none) private(i) shared(mask, length, shift, n, k, in, out, U)
+    int mask = 1 << shift; 
+    int rank_inverse_bit = (first_bit ^ mask) / size; //rank of the process with inverse k-th bit
+
+    if (rank == rank_inverse_bit) 
     {
-        #pragma omp for
-        for (i = 0; i < length; ++i)
+        for (int i = 0; i < size; ++i) 
         {
             int k_bit_to_zero = i & (~ mask);
             int k_bit_to_one = k_bit_to_zero | mask;
             int k_bit = (i & mask) >> shift;
             *(out + i) = *(U + k_bit * 2 + 0) * *(in + k_bit_to_zero) + *(U + k_bit * 2 + 1) * *(in + k_bit_to_one);
+        }
+        
+    } else 
+    {
+        MPI_Sendrecv(in, size, MPI_DOUBLE_COMPLEX, rankChange, 0, out, size, MPI_DOUBLE_COMPLEX, rankChange, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        int k_bit = (first_bit & mask) >> shift;
+        if (k_bit == 1) 
+        { 
+            for (int i = 0; i < size; ++i) 
+            {
+                *(out + i) = *(U + k_bit * 2 + 0) * *(out + i) + *(U + k_bit * 2 + 1) * *(in + i);
+            }
+        } else 
+        {
+            for (int i = 0; i < size; ++i) 
+            {
+                *(out + i) = *(U + k_bit * 2 + 0) * *(in + i) + *(U + k_bit * 2 + 1) * *(out + i);
+            }
         }
     }
     return;
@@ -56,21 +79,21 @@ int main(int argc, char *argv[])
     if (mode == 0) //read vector from the file
     {
         MPI_File data;
-        MPI_File_open(MPI_COMM_WORLD, argv[4], MPI_MODE_RDONLY, MPI_INFO_NULL, &file); //argv[4] is a name of the file
+        MPI_File_open(MPI_COMM_WORLD, argv[4], MPI_MODE_RDONLY, MPI_INFO_NULL, &data); //argv[4] is a name of the file
         
         
         double *buffer = new double[2];
-        int shift = size * rank * sizeof(buffer);
+        int shift = count_of_elements * rank * sizeof(buffer);
         
-        MPI_File_seek(file, shift, MPI_SEEK_SET);
+        MPI_File_seek(data, shift, MPI_SEEK_SET);
         
         for (int i = 0; i < count_of_elements; ++i) 
         {
-            MPI_File_read(file, buffer, 2, MPI_DOUBLE, MPI_STATUS_IGNORE);
+            MPI_File_read(data, buffer, 2, MPI_DOUBLE, MPI_STATUS_IGNORE);
             elements[i] = complexd(buffer[0], buffer[1]);
         }
         
-        MPI_File_close(&file);
+        MPI_File_close(&data);
         delete [] buffer;
     }
     else //random vector
@@ -90,51 +113,90 @@ int main(int argc, char *argv[])
 
         norm = sqrt(norm);
 
-        for (int i = 0; i < size; ++i) 
+        for (int i = 0; i < count_of_elements; ++i) 
         {
             elements[i] = elements[i] / norm;
         }
     }
     
     
-    
-    
-    #pragma omp parallel default(none) private(i) shared(n, k, U, v, length) reduction (+: norm)
-    {
-        #pragma omp for
-        for (i = 0; i < length; ++i)
-        {
-            srand(i);
-            v[i].real(rand());
-            v[i].imag(rand());
-            norm += abs(v[i]) * abs(v[i]);
-        }
-    }
-    norm = sqrt(norm);
-    #pragma omp parallel default(none) private(i) shared(n, k, U, v, norm, length)
-    {
-        #pragma omp for
-        for (i = 0; i < length; ++i)
-        {
-            v[i] = v[i] / norm;
-        }
-    }
-    /*
-    for (i = 0; i < length; ++i)
-    {
-        std::cout << std::endl << v[i];
-    }
-    */
-    complexd *new_v = new complexd[length];
+    complexd *new_elements = new complexd[count_of_elements];
 
     double start, finish;
+    start = MPI_Wtime();
+    OneQubitEvolution(elements, new_elements, U, n, k, count_of_elements);
+    finish = MPI_Wtime();
+    finish -= start;
 
     
-    finish -= start;
-    std::cout << finish << std::endl;
+    #ifdef TEST
+    
+        int correct_flag = 0;
+        //read right vector from the file
+        complexd *right_elements = new complexd[count_of_elements];
+        MPI_File file;
+        MPI_File_open(MPI_COMM_WORLD, "testdata", MPI_MODE_RDONLY, MPI_INFO_NULL, &file); 
+        
+        double *buffer = new double[2];
+        int shift = count_of_elements * rank * sizeof(buffer);
+        
+        MPI_File_seek(file, shift, MPI_SEEK_SET);
+        
+        for (int i = 0; i < count_of_elements; ++i) 
+        {
+            MPI_File_read(file, buffer, 2, MPI_DOUBLE, MPI_STATUS_IGNORE);
+            right_elements[i] = complexd(buffer[0], buffer[1]);
+        }
+        
+        MPI_File_close(&file);
+        delete [] buffer;
+        
+        //compare two vectors
+        int is_right = 1;
+        
+        for (int i = 0; i < count_of_elements; ++i)
+        {
+            if (fabs(new_elements[i].real() - right_elements[i].real()) > EPS || fabs(new_elements[i].imag() - right_elements[i].imag()) > EPS)
+            {
+                is_right = 0;
+                break;
+            }
+        }
+        MPI_Reduce(is_right, correct_flag, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+        
+        if ((rank == 0) && (correct_flag == size))
+        {
+            std::cout << "CORRECT RESULT!" << std::endl;
+        }
+        else
+        {
+            std::cout << "WRONG RESULT!" << std::endl;
+        }
+        
+    #endif
+    
+    MPI_File data;
+    MPI_File_open(MPI_COMM_WORLD, RESFILE, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &data);
+    
+    double *buffer = new double[2];
+    int shift = count_of_elements * rank * sizeof(buffer);
+        
+    MPI_File_seek(data, shift, MPI_SEEK_SET);
+    for (int i = 0; i < count_of_elements; ++i) {
+        buffer[0] = new_elements[i].real();
+        buffer[1] = new_elements[i].imag();
+        MPI_File_write(file, buffer, 2, MPI_DOUBLE, MPI_STATUS_IGNORE);
+    }
+    MPI_File_close(&data);
+    delete [] buffer;
+    
+    
+    std::cout << "TIME: " << finish << std::endl;
     
     delete [] U;
-    delete [] new_v;
+    delete [] new_elements;
+    delete [] elements;
+
     MPI_Finalize();
     return 0;
 }
